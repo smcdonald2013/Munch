c098a516c68ae6775e877c478381a688c2b413bd
smcdonald2013 <smcdonald2013@gmail.com>
2018-02-03 00:19:31 -0600

    Cleaning up rest endpoints.

--- food-app/application.py
+++ food-app/application.py
@@ -4,83 +4,12 @@ import sqlalchemy
 import pandas as pd
 import json
 import datetime 
+import models
 
 application = Flask(__name__, static_url_path='/static')
 CORS(application)
 application.config.from_object('config')
 from config import SQLALCHEMY_DATABASE_URI
-
-def item_lookup(item=None):
-    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
-    if item is not None:   
-        sql_string =  ("SELECT * " 
-                        "FROM Prices as p " 
-                        "WHERE p.Food_Name = %s ")
-        df = pd.read_sql_query(sql_string, engine, params=(item,))
-    else: #If no item is provided, pull all of them
-        sql_string =  ("SELECT * " 
-                       "FROM Prices as p "
-                       "ORDER BY p.Food_Name")
-        df = pd.read_sql_query(sql_string, engine)
-    df['Price_Cooking_Unit'] = df['Price_Purchase_Unit'].divide(df['Num_Cooking_Unit'])
-    return df
-
-def recipe_lookup(recipe=None):
-    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
-    if recipe is not None:   
-        sql_string =  ("SELECT * " 
-                        "FROM Recipes as r "
-                        "WHERE r.Recipe_Name = %s "
-                        "ORDER BY r.Food_Name")
-        df = pd.read_sql_query(sql_string, engine, params=(recipe,))
-    else: #If no recipe is provided, pull all of them
-        sql_string =  ("SELECT * " 
-                        "FROM Recipes as r "
-                        "ORDER BY r.Recipe_Name")
-        df = pd.read_sql_query(sql_string, engine)
-    return df
-
-def list_lookup():
-    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
-    sql_string =  ("SELECT * " 
-                    "FROM Lists as l "
-                    "WHERE DATE_ID = (SELECT MAX(DATE_ID) as latest_date FROM Lists sub)")
-    df = pd.read_sql_query(sql_string, engine)
-    return df
-
-def cost_lookup(recipe=None):
-    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
-    if recipe is not None:   
-        sql_string =  ("SELECT * " 
-                        "FROM Costs as r "
-                        "WHERE r.Recipe_Name = %s "
-                        "ORDER BY r.Food_Name")
-        df = pd.read_sql_query(sql_string, engine, params=(recipe,))
-    else: #If no recipe is provided, pull all of them
-        sql_string =  ("SELECT * " 
-                        "FROM Costs as r "
-                        "ORDER BY r.Recipe_Name")
-        df = pd.read_sql_query(sql_string, engine)
-    return df
-
-def links_lookup(recipe=None):
-    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
-    if recipe is not None:   
-        sql_string =  ("SELECT * " 
-                        "FROM Links as l "
-                        "WHERE l.Recipe_Name = %s "
-                        "ORDER BY l.Recipe_Name")
-        df = pd.read_sql_query(sql_string, engine, params=(recipe,))
-    else: #If no recipe is provided, pull all of them
-        sql_string =  ("SELECT * " 
-                        "FROM Links as l "
-                        "ORDER BY l.Recipe_Name")
-        df = pd.read_sql_query(sql_string, engine)
-    return df
-
-def sql_insert(data_df, table):
-    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
-    data_df.to_sql(table, engine, index=False, if_exists='append')
 
 #Web App URLs (Return html)
 @application.route("/")
@@ -100,10 +29,6 @@ def recipe_update():
 def grocery_list():
     return render_template('grocery_list.html')
 
-@application.route("/edit_list", methods=['GET'])
-def edit_list():
-    return render_template('edit_list.html')
-
 @application.route("/outline", methods=['GET'])
 def outline():
     return render_template('outline.html')
@@ -118,22 +43,13 @@ def get_item():
         data = pd.DataFrame()
         for item in a:
             item_new = str(item).strip('\'') #Need to remove quotes for pymysql to handle strings properly
-            df = item_lookup(item_new)
+            df = models.item_lookup(item_new)
             application.logger.debug(df)
             data = pd.concat([data, df])
-        data_json = data.to_json(orient='records') #Should probably move out of for loop.
-        data_dict = json.loads(data_json)
-        data_dict_final = {'data': data_dict}
-        data_final_json = json.dumps(data_dict_final)
+        data_final_json = json_format(data)
     else:
-        #data_json =pd.DataFrame(item_lookup()['Food_Name']).to_json(orient='records')
-        #data_dict = json.loads(data_json)
-        #data_dict_final = {'data': data_dict}
-        #data_final_json = json.dumps(data_dict_final)
-        data_json =pd.DataFrame(item_lookup()).to_json(orient='records')
-        data_dict = json.loads(data_json)
-        data_dict_final = {'data': data_dict}
-        data_final_json = json.dumps(data_dict_final)
+        data_df = pd.DataFrame(models.item_lookup())
+        data_final_json = json_format(data_df)
     return data_final_json
 
 @application.route('/items', methods=['POST'])
@@ -142,7 +58,7 @@ def create_item():
     application.logger.debug(data)
     data_json = json.loads(data, object_pairs_hook=deunicodify_hook)
     data_df = pd.DataFrame(data_json, index=[0])
-    sql_insert(data_df, table='Prices')
+    models.sql_insert(data_df, table='Prices')
     return data_df.to_json(orient='values')
 
 #Recipe Endpoints
@@ -153,38 +69,15 @@ def get_recipe():
         a = ids.split(",") 
         data = pd.DataFrame()
         for recipe in a:
-            recipe_new = str(recipe).strip('\'') #Need to remove quotes for pymysql to handle strings properly
-            df = recipe_lookup(recipe)
-            application.logger.debug(df)
-            df = df.drop(['ID', 'Recipe_Name'], axis=1)
-            data = pd.concat([data, df])
-            num_df = data.groupby('Food_Name').sum()['Food_Units']
-            text_df = data.drop_duplicates('Food_Name')[['Food_Name', 'Food_Units_Name', 'Food_Alt_Units', 'Food_Alt_Name']]
-            text_df = text_df.sort_values(by='Food_Name').set_index('Food_Name')
-            text_df['Food_Units'] = num_df
-            text_df = text_df.round(decimals=2).reset_index()
-            #text_df = text_df[["Food_Name", "Food_Units", "Food_Units_Name", "Food_Alt_Name", "Food_Units"]]
-            text_df = text_df[["Food_Name", "Food_Units", "Food_Units_Name"]]
-            text_df['Food_Alt_Units'] = ""
-            text_df['Food_Alt_Name'] = ""
-            text_df = text_df[["Food_Name", "Food_Units", "Food_Units_Name", "Food_Alt_Units", "Food_Alt_Name"]]
-        data_json = text_df.to_json(orient='records') #Should probably move out of for loop. 
-        data_dict = json.loads(data_json)
-        data_dict_final = {'data': data_dict}
-        data_final_json = json.dumps(data_dict_final)
+            #recipe_new = str(recipe).strip('\'') #Need to remove quotes for pymysql to handle strings properly
+            df = models.recipe_lookup(recipe)
+            text_df, data = recipe_format(df, data, ['Food_Units_Name'])
+        data_final_json = json_format(text_df)
     else: 
-        #data_df = pd.DataFrame(recipe_lookup()['Recipe_Name'].drop_duplicates())
-        #data_json = data_df.to_json(orient='records')
-        #data_dict = json.loads(data_json)
-        #data_dict_final = {'data': data_dict}
-        #data_final_json = json.dumps(data_dict_final)
-        data_df = pd.DataFrame(recipe_lookup())
+        data_df = pd.DataFrame(models.recipe_lookup())
         data_json = data_df.groupby('Recipe_Name').apply(lambda x: x.to_json(orient='records'))
         data_json = data_json.apply(lambda x: json.loads(x))
-        data_json = data_json.reset_index().to_json(orient='records')
-        data_dict = json.loads(data_json)
-        data_dict_final = {'data': data_dict}
-        data_final_json = json.dumps(data_dict_final)
+        data_final_json = json_format(data_json.reset_index())
     return data_final_json
 
 @application.route('/recipe_cost', methods=['GET'])
@@ -194,30 +87,15 @@ def get_recipe_cost():
         a = ids.split(",") 
         data = pd.DataFrame()
         for recipe in a:
-            recipe_new = str(recipe).strip('\'') #Need to remove quotes for pymysql to handle strings properly
-            df = cost_lookup(recipe)
-            application.logger.debug(df)
-            df = df.drop(['Recipe_Name'], axis=1)
-            data = pd.concat([data, df])
-            num_df = data.groupby('Food_Name').sum()['Food_Units']
-            text_df = data.drop_duplicates('Food_Name')[['Food_Name', 'Food_Units_Name', "Ingredient_Cost"]]
-            text_df = text_df.sort_values(by='Food_Name').set_index('Food_Name')
-            text_df['Food_Units'] = num_df
-            text_df = text_df.round(decimals=2).reset_index()
-            #text_df = text_df[["Food_Name", "Food_Units", "Food_Units_Name", "Food_Alt_Name", "Food_Units"]]
-            text_df = text_df[["Food_Name", "Food_Units", "Food_Units_Name", "Ingredient_Cost"]]
+            #recipe_new = str(recipe).strip('\'') #Need to remove quotes for pymysql to handle strings properly
+            df = models.cost_lookup(recipe)
+            text_df, data = recipe_format(df, data, ['Food_Units_Name', 'Ingredient_Cost'])
             text_df['Total_Costs'] = text_df['Food_Units']*text_df['Ingredient_Cost']
             text_df = text_df[["Food_Name", "Food_Units", "Food_Units_Name", "Ingredient_Cost", "Total_Costs"]]
-        data_json = text_df.to_json(orient='records') #Should probably move out of for loop. 
-        data_dict = json.loads(data_json)
-        data_dict_final = {'data': data_dict}
-        data_final_json = json.dumps(data_dict_final)
+        data_final_json = json_format(text_df)
     else: 
-        data_df = pd.DataFrame(cost_lookup()['Recipe_Name'].drop_duplicates())
-        data_json = data_df.to_json(orient='records')
-        data_dict = json.loads(data_json)
-        data_dict_final = {'data': data_dict}
-        data_final_json = json.dumps(data_dict_final)
+        data_df = pd.DataFrame(models.cost_lookup()['Recipe_Name'].drop_duplicates())
+        data_final_json = json_format(data_df)
     return data_final_json
 
 @application.route('/recipes', methods=['POST'])
@@ -230,20 +108,16 @@ def create_recipe():
     data_df.columns = [x.replace(" ", "_") for x in data_df.columns]
     data_df = data_df[['Recipe_Name','Food_Name', 'Food_Units', 'Food_Units_Name', 'Food_Alt_Units', 'Food_Alt_Name']]
     application.logger.debug(data_df)
-    sql_insert(data_df, table='Recipes')
+    models.sql_insert(data_df, table='Recipes')
     return data_df.to_json(orient='values')
 
 @application.route("/lists", methods=['GET'])
 def get_list():  
-    df = list_lookup()
+    df = models.list_lookup()
     df = df[['Food_Name', 'Num_Cooking_Unit', 'Name_Cooking_Unit']]
     df['Food_Alt_Units'] = ""
     df['Food_Alt_Name'] = ""
-    #data_json = df.to_json(orient='values')
-    data_json = df.to_json(orient='records')
-    data_dict = json.loads(data_json)
-    data_dict_final = {'data': data_dict}
-    data_final_json = json.dumps(data_dict_final)
+    data_final_json = json_format(df)
     return data_final_json
 
 @application.route('/lists', methods=['POST'])
@@ -254,27 +128,20 @@ def create_list():
     application.logger.debug(data_json)
     data_df = pd.DataFrame(data_json)
     application.logger.debug(data_df)
-    #data_df.columns = [x.replace(" ", "_") for x in data_df.columns]
-    #data_df.columns = ['Food_Name', 'Num_Cooking_Unit', 'Name_Cooking_Unit']
-    #data_df.columns = ['Food_Name', 'Num_Cooking_Unit', 'Name_Cooking_Unit', 'Food_Alt_Units', 'Food_Alt_Name']
     data_df = data_df[['Food Name', 'Food Units', 'Food Units Name']]
     data_df.columns = ['Food_Name', 'Num_Cooking_Unit', 'Name_Cooking_Unit']
     data_df['Date_ID'] = datetime.datetime.now()
     data_df = data_df[['Date_ID','Food_Name', 'Num_Cooking_Unit', 'Name_Cooking_Unit']]
     application.logger.debug(data_df)
-    sql_insert(data_df, table='Lists')
-    #sql_insert(data_df, table='ListsNew')
+    models.sql_insert(data_df, table='Lists')
     return data_df.to_json(orient='values')
 
 @application.route('/links', methods=['GET'])
 def get_link():
-    data_df = pd.DataFrame(links_lookup())
+    data_df = pd.DataFrame(models.links_lookup())
     data_json = data_df.groupby('Recipe_Name').apply(lambda x: x.to_json(orient='records'))
     data_json = data_json.apply(lambda x: json.loads(x))
-    data_json = data_json.reset_index().to_json(orient='records')
-    data_dict = json.loads(data_json)
-    data_dict_final = {'data': data_dict}
-    data_final_json = json.dumps(data_dict_final)
+    data_final_json = json_format(data_json.reset_index())
     return data_final_json
 
 ###Helper functions
@@ -292,6 +159,25 @@ def deunicodify_hook(pairs):
             key = key.encode('utf-8')
         new_pairs.append((key, value))
     return dict(new_pairs)
+
+def json_format(data_df):
+    data_json = data_df.to_json(orient='records')
+    data_dict = json.loads(data_json)
+    data_dict_final = {'data': data_dict}
+    data_final_json = json.dumps(data_dict_final)
+    return data_final_json
+
+def recipe_format(df, data, columns):
+    application.logger.debug(df)
+    df = df.drop(['Recipe_Name'], axis=1)
+    data = pd.concat([data, df])
+    num_df = data.groupby('Food_Name').sum()['Food_Units']
+    text_df = data.drop_duplicates('Food_Name')[['Food_Name'] + columns]
+    text_df = text_df.sort_values(by='Food_Name').set_index('Food_Name')
+    text_df['Food_Units'] = num_df
+    text_df = text_df.round(decimals=2).reset_index()
+    text_df = text_df[['Food_Name', 'Food_Units']+columns]
+    return text_df, data
 
 if __name__ == "__main__":  
     application.run(host='0.0.0.0', debug=True, extra_files="/static/rest_funcs.js")
